f46e6a8ff48b9efee6dd4c82530911edbb526b49
Lani <lani.mortontran@movemedical.com>
2022-03-15 11:45:11 -0700

    Convert Containers to use Card and ListTile widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,91 +11,87 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Column(
-          children: [
-            CircleAvatar(
-              radius: 50.0,
-              child: Icon(
-                Icons.face_outlined,
-                size: 50.0,
-                color: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                child: Icon(
+                  Icons.face_outlined,
+                  size: 50.0,
+                  color: Colors.teal,
+                ),
+                backgroundColor: Colors.white,
               ),
-              backgroundColor: Colors.white60,
-            ),
-            Text(
-              'Lani',
-              style: TextStyle(
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Pacifico',
+              Text(
+                'Lani',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
               ),
-            ),
-            Text(
-              'Web Engineer'.toUpperCase(),
-              style: TextStyle(
-                color: Colors.teal.shade100,
-                fontFamily: 'Source Sans Pro',
-                fontSize: 20.0,
-                letterSpacing: 2.0,
-                fontWeight: FontWeight.bold,
+              Text(
+                'Web Engineer'.toUpperCase(),
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 25.0,
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
               ),
-              child: Row(
-                children: [
-                  Icon(
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
                     Icons.phone,
                     color: Colors.teal,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
+                  title: Text(
                     '1 888 847 2773',
                     style: TextStyle(
                       color: Colors.teal.shade900,
                       fontFamily: 'Source Sans Pro',
                       fontSize: 20.0,
                     ),
-                  )
-                ],
+                  ),
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 25.0,
-              ),
-              child: Row(
-                children: [
-                  Icon(
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
                     Icons.email,
                     color: Colors.teal,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
+                  title: Text(
                     'lani@email.com',
                     style: TextStyle(
                       color: Colors.teal.shade900,
                       fontFamily: 'Source Sans Pro',
                       fontSize: 20.0,
                     ),
-                  )
-                ],
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
