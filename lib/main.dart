8f5ddbe8dd949219b46ae13a22cd9c152f5537a1
Lani <lani.mortontran@movemedical.com>
2022-03-14 23:32:42 -0700

    Flutter Layouts Challenge
    https://drive.google.com/uc?export=download&id=1k7oW5qVLJqzw_lEFsOFS_qKmkGjBX1pL

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,48 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Opacity(
+                      opacity: 0.3,
+                      child: Container(
+                          width: 100.0,
+                          height: 100.0,
+                          color: Colors.yellow,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
