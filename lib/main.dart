c74b7069e276457510798e43272af91faad60c95
Lani <lani.mortontran@movemedical.com>
2022-03-15 01:09:08 -0700

    Tapping into Widget Properties
    Circle avatar and name

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,15 +6,33 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Column(
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              child: Icon(
+                Icons.face_outlined,
+                size: 50.0,
+                color: Colors.teal,
+              ),
+              backgroundColor: Colors.white60,
+            ),
+            Text(
+              'Lani',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
